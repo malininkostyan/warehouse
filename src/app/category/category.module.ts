0571fe1c23211e325d7d832ecd8f811bd985e443
kostyan.malinin <kostyan.malinin@yandex.ru>
2019-12-17 21:56:10 +0400

    lab

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 export const ROUTES: Routes = [
   { path: '', component: CategoryComponent}
