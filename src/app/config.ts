eac9bb023ba375702a938f2c950dbe81a7f82d5c
kostyan.malinin <kostyan.malinin@yandex.ru>
2020-01-23 00:11:13 +0400

    lab

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
-const way = 'http://localhost:3001';
-const wayWS = 'ws://localhost:3002';
-//const way = 'https://warehouse73back.herokuapp.com';
-//const wayWS = 'wss://wsswarehouse73.herokuapp.com';
+//const way = 'http://localhost:3001';
+//const wayWS = 'ws://localhost:3002';
+const way = 'https://warehouse73back.herokuapp.com';
+const wayWS = 'wss://wsswarehouse73.herokuapp.com';
 export {
     way,
     wayWS
